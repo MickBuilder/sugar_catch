ebbd656167dd150f720fbaffef0debd2a67d7ecc
Mike <bmikdaam@gmail.com>
2025-09-25 21:12:58 +0200

    chore: Update package name and identifiers across the project
    - Changed package name from `com.example.sugar_catch` to `com.mikebapps.sugarcatch` in Android, iOS, macOS, Linux, and Windows configurations.
    - Updated application identifiers and copyright information to reflect the new package name.
    - Ensured consistency across all platform-specific files for the new naming convention.

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.sugar_catch"
+    namespace = "com.mikebapps.sugarcatch"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.sugar_catch"
+        applicationId = "com.mikebapps.sugarcatch"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
